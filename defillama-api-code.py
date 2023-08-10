ace4b66b4f4751ba564c8036afb1b7c6c36e89ac
iricehasan <hsn.irice@gmail.com>
2023-08-10 03:41:50 +0300

    Stablecoins data fetched

--- defillama-api-code.py
+++ defillama-api-code.py
@@ -8,6 +8,18 @@ import json
 
 BaseURL = 'https://api.llama.fi'
 
+"""
 protocols = requests.get(BaseURL + '/protocols')
 
 print(protocols.json()[0])
+"""
+
+StablecoinsURL = 'https://stablecoins.llama.fi'
+
+stablecoins = requests.get(StablecoinsURL + '/stablecoins/' + '?includePrices=true')
+
+print(stablecoins.json())
+
+
+
+
