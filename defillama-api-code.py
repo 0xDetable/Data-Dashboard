8d51a37fcd1abfa5970bbe4eb96b007a0fc963f9
iricehasan <hsn.irice@gmail.com>
2023-08-10 04:38:15 +0300

    Sorted stablecoins by chains they circulate with peggedUSD

--- defillama-api-code.py
+++ defillama-api-code.py
@@ -20,8 +20,28 @@ stablecoins = requests.get(StablecoinsURL + '/stablecoins/' + '?includePrices=tr
 
 # print(stablecoins.json())
 
-df = pd.DataFrame(stablecoins)
-print(df)
+pegged_assets = stablecoins.json()['peggedAssets']
 
+flattened_data = []
 
+for asset in pegged_assets:
+    for chain, chain_data in asset["chainCirculating"].items():
+        current_data = chain_data.get("current", {})  # Get current data, handle missing key
+        pegged_usd = current_data.get("peggedUSD")
+        flattened_data.append({
+            'asset_id': asset['id'],
+            'asset_name': asset['name'],
+            'symbol': asset['symbol'],
+            'gecko_id': asset['gecko_id'],
+            'pegType': asset['pegType'],
+            'pegMechanism': asset['pegMechanism'],
+            'chain': chain,
+            'current_peggedUSD': pegged_usd
+        })
+
+df = pd.DataFrame(flattened_data)
+sorted_data = df.sort_values('current_peggedUSD', ascending = False)
+sorted_data.reset_index(drop = True, inplace = True)
+
+print(sorted_data.head(10)) # stablecoins with chains they are circulating
 
