703eff33e8f12f1cf2878f807f8c2d69786be5f4
iricehasan <hsn.irice@gmail.com>
2023-11-11 19:11:10 +0300

    Added Infinite Scroll

--- dashboard/src/pages/dashboard/data/Data.tsx
+++ dashboard/src/pages/dashboard/data/Data.tsx
@@ -78,6 +78,7 @@ import DataTable, { TableColumn } from 'react-data-table-component';
 import Card from '@material-ui/core/Card';
 import Papa from 'papaparse';
 import "raw-loader";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface DataRow {
   asset_id: number;
@@ -90,6 +91,24 @@ interface DataRow {
   pegType: string;
   pegMechanism: string;
 }
+
+const formatMarketCap = (marketCap: number | null): string => {
+  if (marketCap === null) {
+    return 'N/A'; // Or any other suitable placeholder for null values
+  }
+
+  if (marketCap >= 1e9) {
+    return (marketCap / 1e9).toFixed(1) + 'b $';
+  } else if (marketCap >= 1e6) {
+    return (marketCap / 1e6).toFixed(1) + 'm $';
+  } else {
+    return marketCap.toLocaleString() + ' $';
+  }
+};
+  
+  const MarketCapCell = ({ value }: { value: number }) => (
+    <div>{formatMarketCap(value)}</div>
+  );
 
 const columns: TableColumn<DataRow>[] = [
   {
@@ -119,8 +138,9 @@ const columns: TableColumn<DataRow>[] = [
   },
   {
     name: "MarketCap",
-    selector: row => row.market_cap,
-    sortable: true
+    cell: (row) => <MarketCapCell value={row.market_cap} />,
+    sortable: true,
+    sortFunction: (a, b) => a - b, // Add a custom sort function
   },
   {
     name: "MarketCap Percentage",
@@ -140,18 +160,19 @@ const columns: TableColumn<DataRow>[] = [
 ];
 
 const Data = () => {
-    const [csvData, setCsvData] = useState<DataRow[]>([]);
-  
-    useEffect(() => {
-        const filePath = require('!!raw-loader!./data.csv').default;
-  
-      Papa.parse(filePath, {
-        header: true,
-        skipEmptyLines: true,
-        dynamicTyping: true,
-        complete: (results: Papa.ParseResult<DataRow>) => {
-          const parsedData = results.data || [];
-          const filteredData = parsedData.map(row => ({
+  const [csvData, setCsvData] = useState<DataRow[]>([]);
+  const [hasMore, setHasMore] = useState(false);
+
+  useEffect(() => {
+    const filePath = require('!!raw-loader!./data.csv').default;
+
+    Papa.parse(filePath, {
+      header: true,
+      skipEmptyLines: true,
+      dynamicTyping: true,
+      complete: (results: Papa.ParseResult<DataRow>) => {
+        const parsedData = results.data || [];
+        const filteredData = parsedData.map(row => ({
             asset_id: row.asset_id,
             asset_name: row.asset_name,
             symbol: row.symbol,
@@ -163,25 +184,36 @@ const Data = () => {
             pegMechanism: row.pegMechanism,
 
           }));
-  
-          setCsvData(filteredData);
-        },
-      });
-    }, []);
-  
-    return (
-      <div className='container'>
-        <Card>
+
+        setCsvData(filteredData);
+        setHasMore(false); // Set hasMore to false as we won't load more data
+      },
+    });
+  }, []);
+
+  return (
+    <Card style={{ overflowY: 'auto'}}>
+        <InfiniteScroll
+          dataLength={csvData.length}
+          next={() => {}} // Empty function, as we don't want to load more data
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+        >
+ 
+        
           <DataTable
             title="Data"
             columns={columns}
             data={csvData}
-            pagination
+            paginationServer
+            paginationTotalRows={csvData.length}
             fixedHeader
           />
-        </Card>
-      </div>
-    );
-  };
-  
-  export default Data;
+        
+
+      </InfiniteScroll>
+    </Card>
+  );
+};
+
+export default Data;
